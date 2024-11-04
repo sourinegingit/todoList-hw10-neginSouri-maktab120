d33d5517d8f0a6aa581adaf2173d175af9bf2351
negin <neginsouri7979@gmail.com>
2024-11-04 09:21:23 +0430

    create modal

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const apiUrl = "https://63e8a6beb120461c6be38ae5.mockapi.io/tasks";
 async function fetchTasks() {
  const response= await fetch(apiUrl);
  const tasks=response.json();
- console.log(tasks);
- 
+//  renderTasks(tasks)
+//  console.log(tasks);
+//  return tasks
 }
 
 
 
 
 
+function openTaskModal(){
+  document.getElementById("taskModal").style.display="block";
+
+}
+function closeTaskModal(){
+  document.getElementById("taskModal").style.display="none";
+  
+}
 fetchTasks();
